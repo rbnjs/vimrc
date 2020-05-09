c496ecf2c144733f67355238c1d8047976fa882c
Ruben Serradas <rubenserradas@gmail.com>
2020-05-10 00:28:47 +0200

    Fix install

--- install.sh
+++ install.sh
@@ -14,30 +14,30 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-if [ -d "~/.vim" ]; then
+if [ -d ~/.vim ]; then
   echo ".vim folder already created"
 else 
   echo "Creating .vim folder"
-  mkdir "~/.vim"
+  mkdir ~/.vim
 fi
 
-if [ -d "~/.vim/bundle" ]; then
+if [ -d ~/.vim/bundle ]; then
   echo "Vim Vundle folder is already created"
 else
   echo "Creating .vim/bundle folder"
-  mkdir "~/.vim/bundle"
+  mkdir ~/.vim/bundle
 fi
 
 if [ -d "~/.vim/bundle/Vundle.vim" ]; then
   echo "Vim Vundle already installed"
 else
   echo "Downloading vim Vundle"
-  git clone "https://github.com/VundleVim/Vundle.vim.git" "~/.vim/bundle/Vundle.vim"
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 echo "Copying .vimrc"
 
-if [ ! -a "$~/.vimrc" ]; then
+if [ ! -a ~/.vimrc ]; then
   cp .vimrc ~ 
 fi
 
@@ -45,11 +45,11 @@ echo "Installing Plugins"
 vim -c "PluginInstall" -c "qa"
 
 echo "Adding UltiSnips"
-if [ ! -d "~/.vim/UltiSnips" ]; then
-  mkdir "~/.vim/UltiSnips"
+if [ ! -d ~/.vim/UltiSnips ]; then
+  mkdir ~/.vim/UltiSnips
 fi
 
 git clone https://github.com/RubenSerradas/snippets.git
 echo "Copying snippets"
-cp ./snippets/UltiSnips/* \~/.vim/UltiSnips/
+cp ./snippets/UltiSnips/* ~/.vim/UltiSnips/
 rm -rf "snippets"
